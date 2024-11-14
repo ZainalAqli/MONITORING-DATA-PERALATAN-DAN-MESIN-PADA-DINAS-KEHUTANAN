6e369dee184095cf0f0b0b5401bcb026af5c286b
ZainalAqli <m.zainalaqli0908@gmail.com>
2024-11-14 20:59:42 +0800

    Add files via upload

---
+++ MonitoringData/src/monitoringdata/MonitoringData.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package monitoringdata;
+
+/**
+ *
+ * @author HP
+ */
+public class MonitoringData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new gui.FrameUtama().setVisible(true);
+    }
+    
+}
